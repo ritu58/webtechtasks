295e181adbf8f97ca5adb7fb2f023443523b526b
ritu58 <rituranid72@gmail.com>
2025-08-19 16:03:36 +0600

    updated

--- Finallab1.php
+++ Finallab1.php
@@ -1,33 +1,25 @@
 <?php
-
-// 1. Area and Perimeter of a Rectangle
-
-$length = 10;
+//1
+$length = 15;
 $width = 5;
 
-$area = $length * $width;
+$rectangle = $length * $width;
 $perimeter = 2 * ($length + $width);
 
-echo "1. Rectangle: <br>";
-echo "Area = $area <br>";
+echo "Rectangle= $rectangle <br>";
 echo "Perimeter = $perimeter <br><br>";
 
-
-
-// 2. VAT Calculation
-
-$amount = 1000;
+//2
+$amount = 1git000;
 $vat = $amount * 0.15;
 
-echo "2. VAT Calculation: <br>";
+echo "VAT Calculation: <br>";
 echo "Amount = $amount, VAT (15%) = $vat <br><br>";
 
 
-
-// 3. Odd or Even
+//3
 $number = 15;
-
-echo "3. Odd or Even: <br>";
+echo "Odd or Even: <br>";
 if ($number % 2 == 0) {
     echo "$number is Even <br><br>";
 } else {
@@ -35,12 +27,9 @@ if ($number % 2 == 0) {
 }
 
 
-
-// 4. Largest Number Among Three
-
+// 4
 $a = 20; $b = 35; $c = 15;
-
-echo "4. Largest Number: <br>";
+echo "Largest Number: <br>";
 if ($a >= $b && $a >= $c) {
     echo "Largest = $a <br><br>";
 } elseif ($b >= $a && $b >= $c) {
@@ -50,10 +39,9 @@ if ($a >= $b && $a >= $c) {
 }
 
 
+// 5. 
 
-// 5. Print Odd Numbers 10-100
-
-echo "5. Odd Numbers between 10-100: <br>";
+echo "Odd Numbers between 10-100: <br>";
 for ($i = 10; $i <= 100; $i++) {
     if ($i % 2 != 0) {
         echo "$i ";
@@ -63,13 +51,12 @@ echo "<br><br>";
 
 
 
-// 6. Search an Element in Array
-
+// 6.
 $arr = array(5, 10, 15, 20, 25);
 $search = 15;
 $found = false;
 
-echo "6. Search in Array: <br>";
+echo " Search in Array: <br>";
 for ($i = 0; $i < count($arr); $i++) {
     if ($arr[$i] == $search) {
         $found = true;
@@ -83,9 +70,9 @@ if (!$found) {
 
 
 
-// 7. Print Shapes with Nested Loop
+// 7
 
-echo "7. Print Shapes:<br>";
+echo "Print Shapes:<br>";
 
 // Shape 1: Stars
 for ($i = 1; $i <= 3; $i++) {
@@ -121,9 +108,9 @@ echo "<br>";
 
 
 
-// 8. 2D Array & Shapes
+// 8. 
 
-echo "8. 2D Array and Shapes: <br>";
+echo " 2D Array and Shapes: <br>";
 
 $arr2D = array(
     array(1, 2, 3, 'A'),
@@ -131,7 +118,6 @@ $arr2D = array(
     array(1, 'D', 'E', 'F')
 );
 
-// Printing array
 for ($i = 0; $i < count($arr2D); $i++) {
     for ($j = 0; $j < count($arr2D[$i]); $j++) {
         echo $arr2D[$i][$j] . " ";
@@ -141,8 +127,6 @@ for ($i = 0; $i < count($arr2D); $i++) {
 
 echo "<br>";
 
-// Shape (like before)
+// Shape 
 echo "Shapes from array: <br>";
-echo "1<br12<br1<brA<brB C<brD E F<br>";
-
-?>
+echo "1<br>12<br>1<br>A<br>B C<br>D E F<br>";
